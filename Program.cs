3510cce0dab85ba530b1a7c47128d2001823de1c
Ida Johansson <ida.johansson90@hotmail.com>
2025-11-04 16:23:29 +0100

    register program with Employee.cs (for employees) and Register.cs (list and print)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var register = new Register();
+
+        while (true)
+        {
+            Console.Write("Ange ditt namn: ");
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+                break;
+
+            Console.WriteLine("Ange antal arbetade timmar: ");
+            string hoursInput = Console.ReadLine();  
+            if (!int.TryParse(hoursInput, out int workedHours))
+            {
+                Console.WriteLine("Fel inmatning av timmar!");
+                continue;
+            }  
+
+            Console.WriteLine("Ange din timlön: ");
+            string wageInput = Console.ReadLine();  
+            if (!int.TryParse(wageInput, out int hourlyWage))
+            {
+                Console.WriteLine("Fel inmatning av timmar!");
+                continue;
+            }  
+                var employee = new Employee(name, workedHours, hourlyWage);
+                register.AddNewEmployee(employee);
+
+                register.printEmployees(); 
+        }
+
+    }
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Personal_register
+
+- Registret ska kunna ta emot och lagra anställda med namn och lön (via inmatning i konsolen, inget krav på lagring).
+- Programmet ska kunna skriva ut registret i konsol.
+- Vilka klasser bör ingå i programmet?
+- Vilka attribut och metoder bör ingå i klasserna?
